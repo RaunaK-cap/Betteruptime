30307f42e86783975b8f50e94881a5bb22107616
Raunak <raunakkumar6437@gmail.com>
2026-03-24 23:34:08 +0530

    redis

--- apps/producer/index.ts
+++ apps/producer/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello via Bun!");
+import { createClient } from "redis";
+
+const main = async () => {
+  const client = await createClient()
+    .on("error", (err) => console.log("Redis Client Error", err))
+    .connect();
+
+  console.log("redis connected");
+  client.destroy();
+};
+
+main();
