14eeb8482ad2e58e18b03101dff691988a0c2df7
Raunak <raunakkumar6437@gmail.com>
2026-03-25 22:25:50 +0530

    redis stream

--- apps/producer/index.ts
+++ apps/producer/index.ts
@@ -1,12 +1,31 @@
+import { prisma } from "db";
 import { createClient } from "redis";
+import dotend from "dotenv";
+
+dotend.config();
 
 const main = async () => {
+  const websitedata: { url: string; id: number }[] =
+    await prisma.website.findMany();
+
   const client = await createClient()
     .on("error", (err) => console.log("Redis Client Error", err))
     .connect();
 
-  console.log("redis connected");
+  console.log("streams started");
+
+  const pipeline = client.MULTI();
+  websitedata.map((data) => {
+    pipeline.xAdd("betteruptime:websitedata", "*", {
+      website: data.url,
+      id: data.id.toString(),
+    });
+  });
+
+  const results = await pipeline.EXEC();
+  console.log("Generated stream entry IDs:", results);
+
   client.destroy();
 };
 
-main();
+setInterval(main, 4000);
