70424cf287be7c1c323f08b147b6ecdbe7e24921
Raunak <raunakkumar6437@gmail.com>
2026-03-29 22:24:30 +0530

    added redis stream fn

--- packages/queuing-system-redis/index.ts
+++ packages/queuing-system-redis/index.ts
@@ -1,6 +1,7 @@
 import { prisma } from "db";
 import { createClient } from "redis";
 import dotenv from "dotenv";
+import { isConstructSignatureDeclaration } from "typescript";
 
 dotenv.config();
 
@@ -9,6 +10,15 @@ const client = createClient().on("error", (err) =>
 );
 
 const STREAM_NAME = process.env.STREAM_NAME!;
+
+type messageType = {
+  id: string;
+  message: {
+    url: string;
+    id: string;
+  };
+  //@ts-ignore
+};
 
 export const xaddbulk = async (websitedata: { url: string; id: number }[]) => {
   await client.connect();
@@ -37,4 +47,40 @@ export const xaddbulk = async (websitedata: { url: string; id: number }[]) => {
     `[${new Date().toISOString()}] Added ${results?.length} entries:`,
     results,
   );
+};
+
+//@ts-ignore
+export const Xreadgroups = async (
+  consumergroup: string,
+  workingId: string,
+): Promise<messageType[] | undefined> => {
+  const res = await client.xReadGroup(
+    consumergroup,
+    workingId,
+    {
+      key: STREAM_NAME,
+      id: ">",
+    },
+    {
+      COUNT: 5,
+    },
+  );
+
+  //@ts-ignore
+  const message = res?.[0]?.message;
+  return message;
+};
+
+export const Xack = async (consumergroup: string, eventID: string) => {
+  await client.xAck(STREAM_NAME, consumergroup, eventID);
+};
+
+export const XackBulk = async (
+  consumergroup: string,
+  eventID: string,
+  eventIDs: string[],
+) => {
+  eventIDs.forEach((data) => {
+    Xack(consumergroup, eventID);
+  });
 };
