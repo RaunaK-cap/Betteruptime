8b8fb95352c351f3b59073a9c6451dec1a1d3982
Raunak <raunakkumar6437@gmail.com>
2026-04-02 21:58:54 +0530

    completed worker logic

--- apps/worker/index.ts
+++ apps/worker/index.ts
@@ -1,1 +1,70 @@
-console.log("Hello via Bun!");
+import { prisma } from "db";
+import { Xreadgroups, XackBulk } from "queuing-system-redis";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const REGION_ID = process.env.REGION_ID!;
+const WORKING_ID = process.env.WORKING_ID!;
+
+if (!REGION_ID) {
+  throw new Error("regionID is not available");
+}
+
+if (!WORKING_ID) {
+  throw new Error("workingID is not available");
+}
+
+async function main() {
+  while (true) {
+    const responses = await Xreadgroups(REGION_ID, WORKING_ID);
+
+    // gotta check type schema here ////
+    const promises = responses?.map((msg) =>
+      fetchwebsite(msg.message.url, msg.message.id),
+    );
+    //@ts-ignore
+    await Promise.all(promises);
+
+    //@ts-ignore
+    XackBulk(
+      REGION_ID,
+      //@ts-ignore
+      responses?.map((id) => id),
+    );
+  }
+}
+
+async function fetchwebsite(url: string, websiteId: string) {
+  return new Promise<void>((resolve, reject) => {
+    const starttime = Date.now();
+
+    fetch(url)
+      .then(async () => {
+        const endtime = Date.now();
+        await prisma.websiteTick.create({
+          data: {
+            Status: "up",
+            Response_time_ms: endtime - starttime,
+            region_id: Number(REGION_ID),
+            website_id: Number(websiteId),
+          },
+        });
+
+        resolve();
+      })
+      .catch(async () => {
+        const endtime = Date.now();
+        await prisma.websiteTick.create({
+          data: {
+            Status: "down",
+            Response_time_ms: endtime - starttime,
+            region_id: Number(REGION_ID),
+            website_id: Number(websiteId),
+          },
+        });
+
+        resolve();
+      });
+  });
+}
